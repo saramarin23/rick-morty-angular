19be3a47f1c271ebd952e1a09e4b000cf702d1d4
saramarin23 <saramarin.econ@gmail.com>
2019-11-23 22:57:15 +0100

    first commit

---
+++ src/app/services/rick-morty.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { RickMortyService } from './rick-morty.service';
+
+describe('RickMortyService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: RickMortyService = TestBed.get(RickMortyService);
+    expect(service).toBeTruthy();
+  });
+});
